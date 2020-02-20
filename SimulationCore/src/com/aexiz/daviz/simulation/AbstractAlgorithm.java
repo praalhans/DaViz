30d2bd4cc9938dc9084ca3b9bf8eec342c99fecc
Wesley Shann <sshann95@outlook.com>
2020-02-20 21:19:12 +0100

    [#11] Revert "[#11] Move shared code from Algorithm class to core module"
    This reverts commit b80ec35c

--- SimulationCore/src/com/aexiz/daviz/simulation/AbstractAlgorithm.java
+++
@@ -1,9 +0,0 @@
-package com.aexiz.daviz.simulation;
-
-public class AbstractAlgorithm {
-    protected Assumption assumption;
-
-    public Assumption getAssumption() {
-        return assumption;
-    }
-}
