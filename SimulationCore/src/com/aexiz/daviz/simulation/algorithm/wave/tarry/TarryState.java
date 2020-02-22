f3149bf23fbaf07751a165ecfa22b61669e5d04f
Wesley Shann <sshann95@outlook.com>
2020-02-22 21:59:30 +0100

    [#11] Extract TarryState to core module

---
+++ SimulationCore/src/com/aexiz/daviz/simulation/algorithm/wave/tarry/TarryState.java
@@ -0,0 +1,40 @@
+package com.aexiz.daviz.simulation.algorithm.wave.tarry;
+
+import com.aexiz.daviz.simulation.Channel;
+import com.aexiz.daviz.simulation.algorithm.information.PropertyBuilder;
+import com.aexiz.daviz.simulation.algorithm.information.state.AbstractAlgorithmState;
+import com.aexiz.daviz.simulation.algorithm.information.state.PropertyVisitor;
+
+import java.util.List;
+import java.util.Map;
+
+public class TarryState extends AbstractAlgorithmState {
+    boolean hasToken;
+    PropertyVisitor state;
+    List<Channel> neighbors;
+
+    @Override
+    public String toString() {
+        return "(" + hasToken + "," + state + "," + neighbors + ")";
+    }
+
+    public void setHasToken(boolean hasToken) {
+        this.hasToken = hasToken;
+    }
+
+    public void setState(PropertyVisitor state) {
+        this.state = state;
+    }
+
+    public void setNeighbors(List<Channel> neighbors) {
+        this.neighbors = neighbors;
+    }
+
+    public void makeProperties() {
+        properties = Map.of(
+                "Has token?", String.valueOf(hasToken),
+                "State", state,
+                "Neighbors", makeNodesProperty(neighbors)
+        );
+    }
+}
--- SimulationFrege/src/com/aexiz/daviz/simulation/algorithm/wave/Tarry.java
+++ SimulationFrege/src/com/aexiz/daviz/simulation/algorithm/wave/Tarry.java
@@ -4,10 +4,8 @@ import com.aexiz.daviz.frege.simulation.Process.TProcessDescription;
 import com.aexiz.daviz.frege.simulation.Set.TSet;
 import com.aexiz.daviz.frege.simulation.algorithm.wave.Tarry.TDUI;
 import com.aexiz.daviz.simulation.AbstractFregeBasicAlgorithm;
-import com.aexiz.daviz.simulation.Channel;
 import com.aexiz.daviz.simulation.FregeAlgorithm;
 import com.aexiz.daviz.simulation.FregeHelper;
-import com.aexiz.daviz.simulation.algorithm.information.PropertyBuilder;
 import com.aexiz.daviz.simulation.algorithm.information.message.MessageInformation;
 import com.aexiz.daviz.simulation.algorithm.information.result.ResultInformation;
 import com.aexiz.daviz.simulation.algorithm.information.state.PropertyVisitor;
@@ -16,8 +14,6 @@ import com.aexiz.daviz.simulation.algorithm.wave.tarry.*;
 import frege.prelude.PreludeBase.TTuple2;
 import frege.prelude.PreludeBase.TTuple3;
 import frege.run8.Thunk;
-
-import java.util.List;
 
 import static com.aexiz.daviz.frege.simulation.algorithm.wave.Tarry.procDesc;
 
@@ -39,46 +35,15 @@ public class Tarry extends AbstractFregeBasicAlgorithm {
     public StateInformation makeAndUnloadState(FregeHelper helper, Object o) {
         FregeAlgorithm.validateParameters(helper, o);
 
-        class TarryState implements StateInformation {
-            boolean hasToken;
-            PropertyVisitor dui;
-            List<Channel> neighbors;
-
-            public String toString() {
-                return "(" + hasToken + "," + dui + "," + neighbors + ")";
-            }
-
-            public void buildProperties(PropertyBuilder builder) {
-                builder.simpleProperty("Has token?", String.valueOf(hasToken));
-                builder.compoundProperty("State", dui);
-                builder.compoundProperty("Neighbors", new PropertyVisitor() {
-                    public void buildProperties(PropertyBuilder builder) {
-                        builder.simpleProperty("", neighbors.size() + " elements");
-                        for (int i = 0, size = neighbors.size(); i < size; i++) {
-                            builder.simpleProperty(i + ":", neighbors.get(i).to.getLabel());
-                        }
-                    }
-                });
-            }
-        }
         @SuppressWarnings("unchecked")
-        TTuple3<Boolean, TDUI, TSet<TTuple2<Integer, Integer>>> st =
-                (TTuple3<Boolean, TDUI, TSet<TTuple2<Integer, Integer>>>) o;
+        TTuple3<Boolean, TDUI, TSet<TTuple2<Integer, Integer>>> st = (TTuple3<Boolean, TDUI, TSet<TTuple2<Integer, Integer>>>) o;
         TarryState result = new TarryState();
-        result.hasToken = st.mem1.call();
+        result.setHasToken(st.mem1.call());
         TDUI dui = st.mem2.call();
-        if (dui.asReceived() != null) {
-            result.dui = new TarryReceived(helper.getChannelByTuple(dui.asReceived().mem1.call()));
-        } else if (dui.asReplied() != null) {
-            result.dui = new TarryReplied(helper.getChannelByTuple(dui.asReplied().mem1.call()));
-        } else if (dui.asUndefined() != null) {
-            result.dui = new TarryUndefined();
-        } else if (dui.asInitiator() != null) {
-            result.dui = new TarryInitiator();
-        } else {
-            throw new Error("Invalid DUI value");
-        }
-        result.neighbors = helper.forEdgeSet(st.mem3.call());
+        result.setState(makeState(helper, dui));
+        result.setNeighbors(helper.forEdgeSet(st.mem3.call()));
+        result.makeProperties();
+
         return result;
     }
 
@@ -91,6 +56,22 @@ public class Tarry extends AbstractFregeBasicAlgorithm {
     @Override
     public TProcessDescription<Object, Object, Object, Object> getProcessDescription(FregeHelper helper) {
         return procDesc(Thunk.lazy(helper.getIdByNode(assumption.getInitiator()))).simsalabim();
+    }
+
+    private PropertyVisitor makeState(FregeHelper helper, TDUI dui) {
+        if (dui.asReceived() != null) {
+            return new TarryReceived(helper.getChannelByTuple(dui.asReceived().mem1.call()));
+        }
+        if (dui.asReplied() != null) {
+            return new TarryReplied(helper.getChannelByTuple(dui.asReplied().mem1.call()));
+        }
+        if (dui.asUndefined() != null) {
+            return new TarryUndefined();
+        }
+        if (dui.asInitiator() != null) {
+            return new TarryInitiator();
+        }
+        throw new Error("Invalid DUI value");
     }
 
 }
