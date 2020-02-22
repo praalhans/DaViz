82158a450b96f7c760d103a46f94c75e9577d67b
Wesley Shann <sshann95@outlook.com>
2020-02-22 15:58:01 +0100

    [#11] Automated reformating

--- SimulationCore/src/com/aexiz/daviz/simulation/algorithm/information/AbstractInformation.java
+++ SimulationCore/src/com/aexiz/daviz/simulation/algorithm/information/AbstractInformation.java
@@ -11,7 +11,7 @@ public abstract class AbstractInformation implements Information {
         if (!(this instanceof MessageInformation ||
                 this instanceof ResultInformation ||
                 this instanceof StateInformation ||
-                this instanceof PropertyVisitor)){
+                this instanceof PropertyVisitor)) {
             throw new Error("Invalid information type. Information classes must implement one specific information interface");
         }
     }
--- SimulationCore/src/com/aexiz/daviz/simulation/algorithm/information/state/AbstractViewpointState.java
+++ SimulationCore/src/com/aexiz/daviz/simulation/algorithm/information/state/AbstractViewpointState.java
@@ -1,8 +1,6 @@
 package com.aexiz.daviz.simulation.algorithm.information.state;
 
 import com.aexiz.daviz.simulation.Viewpoint;
-import com.aexiz.daviz.simulation.algorithm.information.AbstractInformation;
-import com.aexiz.daviz.simulation.algorithm.information.PropertyBuilder;
 
 import java.util.Map;
 
