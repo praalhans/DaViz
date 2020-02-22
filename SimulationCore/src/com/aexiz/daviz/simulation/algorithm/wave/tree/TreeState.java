8bab1309a0ff59c459cdfd862e96b400b31e70e1
Wesley Shann <sshann95@outlook.com>
2020-02-22 22:05:47 +0100

    [#11] Extract TreeState to core module

---
+++ SimulationCore/src/com/aexiz/daviz/simulation/algorithm/wave/tree/TreeState.java
@@ -0,0 +1,32 @@
+package com.aexiz.daviz.simulation.algorithm.wave.tree;
+
+import com.aexiz.daviz.simulation.Channel;
+import com.aexiz.daviz.simulation.algorithm.information.state.AbstractAlgorithmState;
+import com.aexiz.daviz.simulation.algorithm.information.state.PropertyVisitor;
+
+import java.util.List;
+import java.util.Map;
+
+public class TreeState extends AbstractAlgorithmState {
+    List<Channel> neighbors;
+    PropertyVisitor state;
+
+    public String toString() {
+        return "(" + neighbors + "," + state + ")";
+    }
+
+    public void setNeighbors(List<Channel> neighbors) {
+        this.neighbors = neighbors;
+    }
+
+    public void setState(PropertyVisitor state) {
+        this.state = state;
+    }
+
+    public void makeProperties() {
+        properties = Map.of(
+                "State", state,
+                "Neighbors", makeNodesProperty(neighbors)
+        );
+    }
+}
--- SimulationFrege/src/com/aexiz/daviz/simulation/algorithm/wave/Tree.java
+++ SimulationFrege/src/com/aexiz/daviz/simulation/algorithm/wave/Tree.java
@@ -4,17 +4,13 @@ import com.aexiz.daviz.frege.simulation.Process.TProcessDescription;
 import com.aexiz.daviz.frege.simulation.algorithm.wave.Tree.TPS;
 import com.aexiz.daviz.frege.simulation.algorithm.wave.Tree.TUP;
 import com.aexiz.daviz.simulation.AbstractFregeBasicAlgorithm;
-import com.aexiz.daviz.simulation.Channel;
 import com.aexiz.daviz.simulation.FregeAlgorithm;
 import com.aexiz.daviz.simulation.FregeHelper;
-import com.aexiz.daviz.simulation.algorithm.information.PropertyBuilder;
 import com.aexiz.daviz.simulation.algorithm.information.message.MessageInformation;
 import com.aexiz.daviz.simulation.algorithm.information.result.ResultInformation;
 import com.aexiz.daviz.simulation.algorithm.information.state.PropertyVisitor;
 import com.aexiz.daviz.simulation.algorithm.information.state.StateInformation;
 import com.aexiz.daviz.simulation.algorithm.wave.tree.*;
-
-import java.util.List;
 
 import static com.aexiz.daviz.frege.simulation.algorithm.wave.Tree.procDesc;
 
@@ -35,35 +31,13 @@ public class Tree extends AbstractFregeBasicAlgorithm {
     public StateInformation makeAndUnloadState(FregeHelper helper, Object o) {
         FregeAlgorithm.validateParameters(helper, o);
 
-        class TreeState implements StateInformation {
-            List<Channel> neigh;
-            PropertyVisitor state;
-
-            public String toString() {
-                return "(" + neigh + "," + state + ")";
-            }
-
-            public void buildProperties(PropertyBuilder builder) {
-                builder.compoundProperty("State", state);
-                builder.compoundProperty("Neighbors", new PropertyVisitor() {
-                    public void buildProperties(PropertyBuilder builder) {
-                        builder.simpleProperty("", neigh.size() + " elements");
-                        for (int i = 0, size = neigh.size(); i < size; i++) {
-                            builder.simpleProperty(i + ":", neigh.get(i).to.getLabel());
-                        }
-                    }
-                });
-            }
-        }
         TPS st = (TPS) o;
         TreeState result = new TreeState();
-        result.neigh = helper.forEdgeSet(st.mem$neigh.call());
+        result.setNeighbors(helper.forEdgeSet(st.mem$neigh.call()));
         TUP up = st.mem$state.call();
-        if (up.asUndefined() != null) {
-            result.state = new TreeUndefined();
-        } else if (up.asParent() != null) {
-            result.state = new TreeParent(helper.getChannelByTuple(up.asParent().mem1.call()).to);
-        } else throw new Error();
+        result.setState(makeState(helper, up));
+        result.makeProperties();
+
         return result;
     }
 
@@ -77,6 +51,16 @@ public class Tree extends AbstractFregeBasicAlgorithm {
     @Override
     public TProcessDescription<Object, Object, Object, Object> getProcessDescription(FregeHelper helper) {
         return procDesc.call().simsalabim();
+    }
+
+    private PropertyVisitor makeState(FregeHelper helper, TUP up) {
+        if (up.asUndefined() != null) {
+            return new TreeUndefined();
+        }
+        if (up.asParent() != null) {
+            return new TreeParent(helper.getChannelByTuple(up.asParent().mem1.call()).to);
+        }
+        throw new Error();
     }
 
 }
