d05fe51e0c4c55bf25bb3ad09914256d3cd4dda0
Wesley Shann <sshann95@outlook.com>
2020-02-25 18:21:54 +0100

    [#17] Create Java and Frege modules to port both implementations

---
+++ Ports/DaVizJava/src/com/aexis/daviz/DaVizJava.java
@@ -0,0 +1,9 @@
+package com.aexis.daviz;
+
+import com.aexiz.daviz.ControlFrame;
+
+public class DaVizJava {
+    public static void main(String[] args) {
+        ControlFrame.main(args);
+    }
+}
