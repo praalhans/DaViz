0b72f7c14e729aa46948a7aeeb2fabd09c17db3d
Wesley Shann <sshann95@outlook.com>
2020-02-25 20:33:20 +0100

    [#17] Ensure that the application does not run using Dummy simulation

--- SimulationDummy/src/com/aexiz/daviz/simulation/DefaultSimulation.java
+++ SimulationDummy/src/com/aexiz/daviz/simulation/DefaultSimulation.java
@@ -3,6 +3,6 @@ package com.aexiz.daviz.simulation;
 public class DefaultSimulation extends AbstractSimulation{
     @Override
     public void load() {
-
+        throw new Error("This is a Dummy implementation of the simulation! Use Frege or Java port instead");
     }
 }
