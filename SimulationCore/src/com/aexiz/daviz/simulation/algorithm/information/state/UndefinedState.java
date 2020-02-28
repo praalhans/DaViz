04ff2ed73cb17578865f889e67ce56479fc74311
Wesley Shann <sshann95@outlook.com>
2020-02-28 14:41:41 +0100

    [#18] Revert "[#18] [#25] Fix UndefinedState label to display `Undefined` instead of `Initiator`"
    This reverts commit 99a0ca7f

--- SimulationCore/src/com/aexiz/daviz/simulation/algorithm/information/state/UndefinedState.java
+++ SimulationCore/src/com/aexiz/daviz/simulation/algorithm/information/state/UndefinedState.java
@@ -2,6 +2,6 @@ package com.aexiz.daviz.simulation.algorithm.information.state;
 
 public class UndefinedState extends AbstractState {
     public UndefinedState() {
-        super("Undefined");
+        super("Initiator");
     }
 }
